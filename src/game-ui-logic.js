12c1f114d3754dbf5b95b3099174bc5956ee0b4d
Charley McGowan and Michael Brown <charley@>
2019-01-15 16:29:25 -0800

    properly setup module, add basic gamestate setup

--- src/game-ui-logic.js
+++ src/game-ui-logic.js
@@ -1,18 +1,32 @@
 import { Game } from './game.js';
 
-default class Game {
+class GameUi extends Game{
 
-  function setupGameBoard() {
+  setupGameBoard() {
     for (var infectionStrengthIndex = 3; infectionStrengthIndex >= 1; infectionStrengthIndex--) {
       for (var drawInfectionCardsIndex = 1; drawInfectionCardsIndex <= 1; drawInfectionCardsIndex++) {
         this.flipInfectionCard(infectionStrengthIndex);
       }
     }
+    const players = this.playerList.length;
+    let drawNumber = 0;
+    if(players >= 4) {
+      drawNumber = 2
+    } else if (players ==3 ) {
+      drawNumber = 3;
+    } else {
+      drawNumber = 4;
+    }
+    this.playerList.forEach((player) =>{
+      for(let i = 0; i < drawNumber; i++) {
+        this.drawPlayerCard(player.name);
+      }
+    });
   }
 
-  function gameStats() {
+  gameStats() {
     this.cityList.forEach(function(city) {
-      console.log(`${city.name}'s infections:'`);
+      console.log(`${city.name}'s infections:`);
       city.diseases.forEach(function(disease) {
         if(disease.cubes>0){
           console.log(`   ${disease.codename}: ${disease.cubes}`);
@@ -27,8 +41,5 @@ default class Game {
     });
   }
 
-  }
-
 }
-
-export { Game }
+ export { GameUi }
--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Game } from './game-ui-logic.js';
+import { GameUi } from './game-ui-logic.js';
 import { Player } from './player.js';
 
 
-$(document).ready(function() {
+function createGame(){
   let player1 = new Player('Michael');
   let player2 = new Player('Charley');
-  let newGame = new Game([ player1, player2 ]);
-
+  let newGame = new GameUi([ player1, player2 ]);
+  newGame.gameStats();
+  debugger;
+}
+$(document).ready(function() {
+  createGame();
 });
